4dc1854b5fd4376647949f2166dac6abfafa8029
Luke VanDeGrift <47366946+lvanAttemptTo@users.noreply.github.com>
2023-03-10 15:25:56 -0800

    Add files via upload

--- R/Program.R
+++ R/Program.R
@@ -12,7 +12,7 @@ consoleWriteString <- ""
 speciesTibble <- rebird:::tax
 lati <- 45.5
 long <- -122.6
-species <- "house sparrow"
+species <- ""
 searchSpeciesTibble <- function(columnSearch, columnOutput, commonName, indexOut)
 {
   code <- ""
@@ -20,7 +20,7 @@ searchSpeciesTibble <- function(columnSearch, columnOutput, commonName, indexOut
   
   for (i in 1:16743)
   {
-    if (tolower(speciesTibble[i, columnSearch]) == commonName)
+    if (tolower(speciesTibble[i, columnSearch]) == tolower(commonName))
     {
       code <- toString(speciesTibble[i, columnOutput])
       place <- i
@@ -59,19 +59,20 @@ print(speciesTibble[birdIndex, 1:15])
 
 findClosestSighting <- function(speciesCode)
 {
-  closestSighting <- nearestobs(species = speciesCode, lat = lati, lng = long, key = APIkey)[1,1:13]
+  closestSighting <- nearestobs(species = speciesCode, lat = lati, lng = long, key = APIkey)
+  print(closestSighting)
   R <- 6371
   phi1 <- lati * pi/180
-  phi2 <- closestSighting[1,8] * pi/18
+  phi2 <- closestSighting[1,8] * pi/180
   deltaPhi <- abs(closestSighting[1,8]-lati) * pi/180
   deltaLamda <- abs(closestSighting[1,9]-long) * pi /180
   
   a <- (sin(deltaPhi/2)*sin(deltaPhi/2)) + (cos(phi1) * cos(phi2) * sin(deltaLamda/2)* sin(deltaLamda/2))
   c <- 2 * atan(sqrt(a)/sqrt(1-a))
-  distanceOfClosestSighting = R * c
+  distanceOfClosestSighting <- NaN
+  distanceOfClosestSighting <- R * c
   return(distanceOfClosestSighting)
 }
-
 
 
 
@@ -79,13 +80,24 @@ findClosestSighting <- function(speciesCode)
 print("/n/n/n")
 
 ui <- fluidPage(
-  textInput("text", h3("TextInput"), value = "Enter Species Name"),
-  textOutput("Hello")
+  sidebarLayout(
+    sidebarPanel(
+      textInput("text", h3("TextInput"), value = "Enter Species Name"),
+      textOutput("Hello")
+    ),
+    mainPanel(
+      
+    )
+  )
 )
 server <- function(input, output)
 {
   output$Hello <- renderText({ 
-    paste(findClosestSighting(searchSpeciesTibble(2,3,input$text,FALSE)))
+    inputText <- input$text
+    index <- searchSpeciesTibble(2,1,inputText, TRUE)
+    paste("Closest Sighting Distance (km):", findClosestSighting(searchSpeciesTibble(2,3,inputText,FALSE)),
+          "\n Scientific Name:", speciesTibble[index, 1],
+          "\n Family:", speciesTibble[index, 11], sep = " ")
   })
 }
 
