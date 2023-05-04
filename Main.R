7210a930393f27d0e790079464fb73334f0ca2f9
Luke VanDeGrift <47366946+lvanAttemptTo@users.noreply.github.com>
2023-05-04 13:21:45 -0700

    Add files via upload

--- Main.R
+++ Main.R
@@ -1,4 +1,5 @@
 library("httr")
+library("stringr")
 
 
 nearbyObs <- function(key = NA, lat = NA, lng = NA, dist = 25, back = 14, cat = "all", hotspot = FALSE, includeProvisional = FALSE, sort = "date", sppLocale = "en", maxResults = "all")
@@ -12,28 +13,55 @@ nearbyObs <- function(key = NA, lat = NA, lng = NA, dist = 25, back = 14, cat = 
         
         res <- VERB("GET", url = paste0("https://api.ebird.org/v2/data/obs/geo/recent?lat=", lat, "&lng=", lng, "&sort=", sort), add_headers(headers))
         
-        catRes <- content(res, 'parsed')
-        df <- data.frame(speciesCode = NA, comName = NA, sciName = NA, locId = NA, obsDt = NA, howMany = NA, lat = NA, lng = NA, obsValid = NA, obsReviewed = NA, locationPrivate = NA, subId = NA)
-        print(df)
-        for (i in 1:length(catRes))
+        catRes <- content(res, 'text')
+        parsedSightings <- unlist(str_split(catRes[[1]], "[}]"))
+        parsedSightings <- parsedSightings[!duplicated(parsedSightings)]
+        print(parsedSightings[[1]])
+        parsedSightingInformation <-data.frame(speciesCode = NA, comName = NA, sciName = NA, locId = NA, locName = NA, obsDt = NA, howMany = NA, lat = NA, lng = NA, obsValid = NA, obsReviewed = NA, locationPrivate = NA, subId = NA)
+        
+        for (i in 1:length(parsedSightings))
         {
-            speciesCode1 <- catRes[[i]]$speciesCode
-            comName1 <- catRes[[i]]$comName
-            sciName1 <- catRes[[i]]$sciName
-            locId1 <- catRes[[i]]$locId
-            obsDt1 <- catRes[[i]]$obsDt
-            howMany1 <- catRes[[i]]$howMany
-            lat1 <- catRes[[i]]$lat
-            lng1 <- catRes[[i]]$lng
-            obsValid1 <- catRes[[i]]$obsValid
-            obsReviewed1 <- catRes[[i]]$obsReviewed
-            locationPrivate1 <- catRes[[i]]$locationPrivate
-            subId1 <- catRes[[i]]$subId
-            print(c(speciesCode, comName, sciName, locId, obsDt, howMany, lat, lng, obsValid, obsReviewed, locationPrivate, subId))
-            df2 <- data.frame(speciesCode = speciesCode1, comName = comName1, sciName = sciName1, locId = NA, obsDt = NA, howMany = NA, lat = NA, lng = NA, obsValid = NA, obsReviewed = NA, locationPrivate = NA, subId = NA)
-            df<- rbind(df, )
+            testParse1 <- unlist(str_split(parsedSightings[i], "[{]"))[2]
+            print(testParse1)
+            testParse2 <- unlist(str_split(testParse1, ',\"'))
+            print(testParse2)
+            testParse3 <- c()
+            for (j in testParse2)
+            {
+                p <- unlist(str_split(j, "[:]"))[2]
+                
+                p <- gsub('"', "", p)
+                
+                if (p == "true")
+                {
+                    p <- TRUE
+                }
+                else if (p == "false")
+                {
+                    p <- FALSE
+                }
+                try(
+                    
+                    if (!is.na(as.numeric(p)))
+                    {
+                        p <- as.numeric(p)
+                    }
+                    ,
+                    silent = TRUE
+                )
+                
+                testParse3 <- append(testParse3, p)
+            }
+            print(testParse3)
+            parsedSightingInformation[i,] <- testParse3
         }
-        return(df)
+        # df <- data.frame(speciesCode = NA, comName = NA, sciName = NA, locId = NA, locName = NA, obsDt = NA, howMany = NA, lat = NA, lng = NA, obsValid = NA, obsReviewed = NA, locationPrivate = NA, subId = NA)
+        # for (i in 1:length(catRes))
+        # {
+        #     
+        #     df[i,] <- catRes[[i]]
+        # }
+        # return(df)
     }
     else if (is.na(key))
     {
@@ -49,5 +77,4 @@ nearbyObs <- function(key = NA, lat = NA, lng = NA, dist = 25, back = 14, cat = 
     }
 }
 
-print(nearbyObs(key = "vmgu1o6c6ihc", lat = 45.5896568645855, lng = -122.738592624664))
-warnings()
+nearbyObs(key = "vmgu1o6c6ihc", lat = 45.5896568645855, lng = -122.738592624664)
